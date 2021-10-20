38a6862d3acbdb4ce37d916624c9cc60e4a0b2e5
KrishnaMoorthy12 <akrishnamoorthy007@gmail.com>
2021-10-20 22:57:59 +0530

    fix: updated Configuration interface

--- src/services/config.service.ts
+++ src/services/config.service.ts
@@ -6,7 +6,7 @@ import prettyJson from '../utilities/prettyJson';
 
 export interface Configurations {
   authToken?: string;
-  loggedInAt?: Date;
+  refreshToken?: string;
   specialAccessToken?: string;
   noOfLocalBackups: number;
   preferredEditor: string;
@@ -15,7 +15,7 @@ export interface Configurations {
 
 const CONFIGURATION_FILE = path.resolve(os.homedir(), '.tssrc');
 
-export function addConfiguration(configuration: { [key: string]: any }) {
+export function addConfigurations(configuration: { [key: string]: any }) {
   try {
     const tssrc = fs.readFileSync(CONFIGURATION_FILE, 'utf8');
     const config = { ...JSON.parse(tssrc), ...configuration };
