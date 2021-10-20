92deccaf986d8130d32c9203af18a6fbbe45fd01
KrishnaMoorthy12 <akrishnamoorthy007@gmail.com>
2021-10-20 23:20:12 +0530

    refactor: removed getAuthToken & getRefreshToken apis from tokenHandler in favor of getConfiguration

--- src/utilities/tokenHandler.ts
+++ src/utilities/tokenHandler.ts
@@ -1,28 +1,11 @@
-import fs from 'fs';
-import os from 'os';
-import path from 'path';
-
 import jwt, { JwtPayload } from 'jsonwebtoken';
 import { ConfigurationError } from '../errors';
 import { getConfiguration } from '../services/config.service';
-
-const CONFIGURATION_FILE = path.resolve(os.homedir(), '.tssrc');
 
 export interface TokenPayload extends JwtPayload {
   id: string;
   display_name: string;
   unverified: boolean;
-}
-
-export function getAuthToken() {
-  try {
-    const tssrc = fs.readFileSync(CONFIGURATION_FILE, 'utf8');
-    const config = JSON.parse(tssrc);
-
-    return config.authToken;
-  } catch (error) {
-    throw new ConfigurationError(error as Error);
-  }
 }
 
 export function getTokenPayload() {
